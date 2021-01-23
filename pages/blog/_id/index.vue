1271e6d3abd24216d57fab4b4c15e67a10876b02
saslavik <saslavik@gmail.com>
2021-01-23 16:09:07 +0200

    single blog added

---
+++ pages/blog/_id/index.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="wrapper-content wrapper-content--fixed">
+    <section class="post">
+      <div class="container">
+
+        <!-- header -->
+        <div class="post-header">
+          <img :src="post.img" :alt="post.title">
+          <h2 class="title">{{ post.title }}</h2>
+          <p>{{ post.descr }}</p>
+        </div>
+
+        <!-- body -->
+        <div class="post-body">
+          <p>{{ post.content }}</p>
+        </div>
+
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      post: {
+        id: 1,
+        title: '1 post',
+        descr:'Далеко-далеко за словесными, горами в стране гласных и согласных живут рыбные тексты.',
+        img: 'https://lawnuk.com/wp-content/uploads/2016/08/sprogs-dogs.jpg',
+        content: 'Далеко-далеко за словесными горами в стране гласных и согласных живут рыбные тексты. Путь решила, великий он выйти ему все, последний возвращайся эта коварный, переулка снова рукописи буквоград меня его рукопись даже наш.'
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.post {
+  max-width: 900px;
+  margin: 0 auto;
+}
+
+.post-header {
+  text-align: center;
+  margin: 30px 0;
+  img {
+    max-width: 400px;
+    margin-bottom: 16px;
+  }
+  p {
+    color: #999999;
+  }
+}
+.post-body {
+  text-align: left;
+}
+</style>
---
+++ pages/blog/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="wrapper-content wrapper-content--fixed">
+    <div class="container">
+      <intro title="My posts: "/>
+      <postsList :posts="posts" />
+    </div>
+  </div>
+</template>
+
+<script>
+import intro from '@/components/UI/Intro.vue';
+import postsList from '@/components/Blog/PostsList.vue';
+
+export default {
+  components: {
+    intro,
+    postsList 
+  },
+  data() {
+    return {
+      posts: [
+        {
+          id: 1,
+          title: '1 post',
+          descr:'Далеко-далеко за словесными, горами в стране гласных и согласных живут рыбные тексты.',
+          img: 'https://lawnuk.com/wp-content/uploads/2016/08/sprogs-dogs.jpg'
+        },
+        {
+          id: 2,
+          title: '2 post',
+          descr: 'Далеко-далеко за словесными горами в стране гласных, и согласных живут рыбные тексты.',
+          img: 'https://iheartdogs.com/wp-content/uploads/2017/09/Brite-Bite-0021-2.jpg'
+        },
+        {
+          id: 3,
+          title: '3 post',
+          descr:'Далеко-далеко за словесными, горами в стране гласных и согласных живут рыбные тексты.',
+          img: 'https://img.gazeta.ru/files3/289/7945289/Klondike_playing-pic905-895x505-44980.jpg'
+        }
+      ]
+    }
+  }
+}
+</script>
